8572c9725a5052997c9cc685018da4121444cd85
Faiyaz1994 <faiyaz1994@gmail.com>
2023-09-02 21:50:47 +0600

    storing and adding codes added

--- Variables_Data_Types.js
+++ Variables_Data_Types.js
@@ -7,3 +7,12 @@ var numberTwo= '16'
 numberTwo= parseInt(numberTwo);
 console.log(numberOne+numberTwo);
 
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
